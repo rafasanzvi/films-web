55ce71bfd88372ce357c602b2faa40375f66ef2e
rafasanzvi <rafasv41@gmail.com>
2022-09-15 20:42:06 +0200

    Getting movies from free API

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,7 +6,14 @@ export function MovieDetails() {
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path
     return (
         <div className={styles.detailsContainer}>
-            <img src={imageUrl} alt="" />
+            <img className={`${styles.col} ${styles.movieImage}`} src={imageUrl} alt={movie.title} />
+            <div className={`${styles.col} ${styles.movieDetails}`}>
+                <p className={styles.firstItem}><strong>Title:</strong> {movie.title}</p>
+                <p>
+                    <strong>Genres:</strong> {movie.genres.map(genre => genre.name).join(", ")}
+                </p>
+                <p><strong>Description:</strong> {movie.overview}</p>
+            </div>
         </div>
     )
 }
